277d67d4bd5eb3e8dd3f98d6bec37b44c838846a
= <=>
2023-12-14 12:25:10 -0600

    readme

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 #### This project uses MySql as a database, so it should be installed in order to work.
 
-#### Go to the storage.properties files stored inside:
+#### Go to the storage.properties file stored inside:
 * src/main/resources/storage.properties
 
 [//]: # (#### Make sure to change the port, hostname, username and password variables to the ones needed for your machine)
@@ -15,3 +15,20 @@
 
 #### so make sure to change them in order to meet the values for your machine.
 #### By default, port will have a value of 3306 and hostname will have localhost.
+
+### Steps to run the schemas:
+In the resources folder under the path:
+* src/main/resources/ddl.sql
+
+there will be a file containing the ddl script to create the trainee, trainer, training, trainingType and user tables;
+it also creates the default insert values for the training types table.
+
+Or enable these properties in the 'props' bean in the JpaBeanConfiguration:
+
+```props.put("hibernate.hbm2ddl.import_files", "trainingTypes.sql");```
+
+```props.put("hibernate.hbm2ddl.auto", "create-drop");```
+
+in order to allow automated creation of the tables based on the entity classes.
+
+
--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,63 +1,67 @@
+-- gym database
+CREATE DATABASE gym;
+
 -- Users table
-
 CREATE TABLE user(
-    id INT NOT NULL AUTO_INCREMENT,
-    isActive bit(1) NOT NULL,
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    PRIMARY KEY(`id`)
-);
-
--- Trainees table
-
-CREATE TABLE trainee(
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
-    dateOfBirth DATETIME(6) DEFAULT NULL,
-    address VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY(id),
-    UNIQUE KEY trainee_user_uk (user_id),
-    CONSTRAINT trainee_user_fk FOREIGN KEY (user_id) REFERENCES `user` (id)
-);
-DROP TABLE trainee;
--- Trainers table
-
-CREATE TABLE trainer(
-    id INT NOT NULL AUTO_INCREMENT,
-    specialization INT NOT NULL,
-    user_id INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY user_uk (user_id),
-    CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES `user`(id),
-    CONSTRAINT specialization_fk FOREIGN KEY (specialization) REFERENCES trainingtype (id)
+   id INT NOT NULL AUTO_INCREMENT,
+   isActive bit(1) NOT NULL,
+   firstName VARCHAR(255) NOT NULL,
+   lastName VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   username VARCHAR(255) NOT NULL,
+   PRIMARY KEY(`id`)
 );
 
 -- TrainingTypes table
-
 CREATE TABLE trainingtype (
-    id INT NOT NULL AUTO_INCREMENT,
-    trainingTypeName VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (id)
+   id INT NOT NULL AUTO_INCREMENT,
+   trainingTypeName VARCHAR(255) DEFAULT NULL,
+   PRIMARY KEY (id)
+);
+
+-- Trainees table
+CREATE TABLE trainee(
+   id INT NOT NULL AUTO_INCREMENT,
+   user_id INT NOT NULL,
+   dateOfBirth DATETIME(6) DEFAULT NULL,
+   address VARCHAR(255) DEFAULT NULL,
+   PRIMARY KEY(id),
+   UNIQUE KEY trainee_user_uk (user_id),
+   CONSTRAINT trainee_user_fk FOREIGN KEY (user_id) REFERENCES `user` (id)
+);
+
+-- Trainers table
+CREATE TABLE trainer(
+   id INT NOT NULL AUTO_INCREMENT,
+   specialization INT NOT NULL,
+   user_id INT NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE KEY user_uk (user_id),
+   CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES `user`(id),
+   CONSTRAINT specialization_fk FOREIGN KEY (specialization) REFERENCES trainingtype (id)
 );
 
 -- Trainings table
-
 CREATE TABLE training(
-    id INT NOT NULL AUTO_INCREMENT,
-    trainee_id INT NOT NULL,
-    trainer_id INT NOT NULL,
-    trainingDuration DOUBLE NOT NULL,
-    trainingType_id INT NOT NULL,
-    trainingDate DATETIME(6) DEFAULT NULL,
-    trainingName VARCHAR(255) DEFAULT NULL,
-    PRIMARY KEY (id),
-    KEY trainee_fk (trainee_id),
-    KEY trainer_fk (trainer_id),
-    KEY trainingType_fk (trainingType_id),
-    CONSTRAINT trainingType_fk FOREIGN KEY (trainingType_id) REFERENCES trainingtype (id),
-    CONSTRAINT trainer_fk FOREIGN KEY (trainer_id) REFERENCES trainer (id),
-    CONSTRAINT trainee_fk FOREIGN KEY (trainee_id) REFERENCES trainee (id)
+   id INT NOT NULL AUTO_INCREMENT,
+   trainee_id INT NOT NULL,
+   trainer_id INT NOT NULL,
+   trainingDuration DOUBLE NOT NULL,
+   trainingType_id INT NOT NULL,
+   trainingDate DATETIME(6) DEFAULT NULL,
+   trainingName VARCHAR(255) DEFAULT NULL,
+   PRIMARY KEY (id),
+   KEY trainee_fk (trainee_id),
+   KEY trainer_fk (trainer_id),
+   KEY trainingType_fk (trainingType_id),
+   CONSTRAINT trainingType_fk FOREIGN KEY (trainingType_id) REFERENCES trainingtype (id),
+   CONSTRAINT trainer_fk FOREIGN KEY (trainer_id) REFERENCES trainer (id),
+   CONSTRAINT trainee_fk FOREIGN KEY (trainee_id) REFERENCES trainee (id)
 );
 
+-- Training Type values
+INSERT INTO TrainingType (id, trainingTypeName) values (1, 'Cardiovascular Training');
+INSERT INTO TrainingType (id, trainingTypeName) values (2, 'Strength Training');
+INSERT INTO TrainingType (id, trainingTypeName) values (3, 'Yoga');
+INSERT INTO TrainingType (id, trainingTypeName) values (4, 'Cross-Fit');
+INSERT INTO TrainingType (id, trainingTypeName) values (5, 'Pilates');
